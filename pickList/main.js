e5864f9df7aab902e5df12e24d2381737a541f4f
Charles Moss <charlesamoss@yahoo.com>
2018-04-07 11:38:12 -0700

    add removel of disabled and disabled alert

--- pickList/main.js
+++ pickList/main.js
@@ -1,23 +1,26 @@
 const Pickable = document.getElementById('list-container-pickable');
-const PickableItems = Array.from(Pickable.querySelectorAll('li'));
-
 const Picked = document.getElementById('list-container-picked');
-const PickedItems = Array.from(Picked.querySelectorAll('li'));
 
 function pick(elem) {
     let elemContent = elem.firstChild.innerHTML;
     if(!elem.classList.contains('disabled')) {
         let removeable = `<li class="list-group-item" onclick="remove(this)"><span>${elemContent}</span></li>`;
         Picked.insertAdjacentHTML('beforeend', removeable);
-        elem.classList.add("disabled");
+        elem.classList.add('disabled');
     } else {
         console.log(`${elemContent} has already been picked`);
+        let pickedElem = Array.from(Picked.querySelectorAll('li'))
+            .find(el => el.textContent === elem.firstChild.innerHTML);
+        pickedElem.classList.add('list-group-item-info'); 
+        setTimeout(function(){
+            pickedElem.classList.remove('list-group-item-info');
+        }, 500);
     }
 }
 
 function remove(elem) {
-    Picked.removeChild(elem)
-    //let disabledPick = Pickable.querySelectorAll('li').innerText = elem.firstChild.innerHTML;
-    //console.log(disabledPick)
-    //disabledPick.classList.remove("disabled")
+    Picked.removeChild(elem);
+    Array.from(Pickable.querySelectorAll('li'))
+        .find(el => el.textContent === elem.firstChild.innerHTML)
+        .classList.remove("disabled");
 }
